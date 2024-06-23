f178a80cc97d0d043bfc262e9058063a9836e1d5
rishabh06122001 <129362415+rishabh06122001@users.noreply.github.com>
2024-06-23 19:59:57 +0530

    Update AstroloCouncellorController.js

--- Routes/AstroloCouncellorController.js
+++ Routes/AstroloCouncellorController.js
@@ -39,7 +39,7 @@ const upload = multer({
 });
 
 const AstrologerController = require("../Controller/AstroloCouncellorController");
-const { registerValidator, loginValidation } = require("../helper/validation");
+const { registerValidator, loginValidation } = require("../helper/validation.js");
 
 // Route for astrologer registration
 router.post(
